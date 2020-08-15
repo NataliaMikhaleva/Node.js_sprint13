8b9682f3b66671fcf50bee2ee78a1c3157e32f99
Natalia <mihaleva.natalya2018@yandex.ru>
2020-08-15 19:30:28 +0300

    added models and routes

--- data/app.js
+++ data/app.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 app.use(express.static((path.join(__dirname, 'public'))));
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5f37b10b737bf835d4ce1953',
-  };
-
-  next();
-});
 app.use('/users', users);
 app.use('/cards', cards);
 app.use((req, res) => {
@@ -29,5 +13,4 @@ app.use((req, res) => {
 });
 app.listen(PORT, () => {
   console.log(`Сервер запущен, порт: ${PORT}.`);
-  console.log(BASE_PATH);
 });
